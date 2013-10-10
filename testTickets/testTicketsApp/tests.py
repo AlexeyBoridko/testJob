1ed248a0e8b121976a05933db4b7740d6fb024bd
AlexeyBoridko <oleksiyua@gmail.com>
2013-10-10 18:25:48 +0300

    ticket:1 tests is added

--- testTickets/testTicketsApp/tests.py
+++ testTickets/testTicketsApp/tests.py
@@ -6,11 +6,26 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from testTicketsApp.models import UserInfo
+from django.test.client import Client
+from django.core.urlresolvers import reverse
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+    fixtures = [
+        'testTicketsApp/fixtures/initial_data.json',
+        ]
+
+    def test_model(self):
+        m = UserInfo.objects.get(pk=1)
+        self.assertEqual(m.name, "Alexey")
+        self.assertEqual(m.surname, "Boridko")
+        self.assertEqual(m.email, "my@email.com")
+        self.assertEqual(m.contacts, "Location - Kherson")
+
+
+    def test_view(self):
+        client = Client()
+        response = client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<p>alexey@42cc.co</p>")
