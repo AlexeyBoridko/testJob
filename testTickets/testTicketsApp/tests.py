6cfa954e44661cede0e21474d96edf602d93378b
AlexeyBoridko <oleksiyua@gmail.com>
2013-10-11 17:54:52 +0300

    ticket:3 added tests

--- testTickets/testTicketsApp/tests.py
+++ testTickets/testTicketsApp/tests.py
@@ -9,6 +9,7 @@ from django.test import TestCase
 from testTicketsApp.models import UserInfo
 from django.test.client import Client
 from django.core.urlresolvers import reverse
+from  .models import MiddlewareRequests
 
 
 class SimpleTest(TestCase):
@@ -30,3 +31,29 @@ class SimpleTest(TestCase):
         response = client.get(reverse('main'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "<p>alexey@42cc.co</p>")
+
+
+    def test_db_for_requests_empty(self):
+        emptyList = MiddlewareRequests.objects.all()
+        self.assertEqual(emptyList.count(), 0)
+
+
+    def test_request_object_create(self):
+        MiddlewareRequests.objects.create(host="127.0.0.1:8000", path="/testonly/", method="GET")
+        mList = MiddlewareRequests.objects.all()
+        self.assertEqual(mList.count(), 1)
+
+        m = MiddlewareRequests.objects.get(pk=1)
+        self.assertEqual(m.host, "127.0.0.1:8000")
+        self.assertEqual(m.path, "/testonly/")
+        self.assertEqual(m.method, "GET")
+
+
+    def test_request_tracker_middleware_full_cycle(self):
+        client = Client()
+        response = client.get(reverse('requests'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<td>/requests/</td>")
+
+
+
