6714cf16d6cd8a5a29a3dd6e02e6ffe8396af53a
AlexeyBoridko <oleksiyua@gmail.com>
2013-10-11 16:57:54 +0300

    ticket:3 added new class for storing requests data

--- testTickets/testTicketsApp/models.py
+++ testTickets/testTicketsApp/models.py
@@ -13,5 +13,13 @@ class UserInfo(models.Model):
     other_contacts = models.TextField(default="")
     contacts = models.CharField(max_length=100, default="")
 
+
     def __str__(self):
         return self.name
+
+
+class MiddlewareRequests(models.Model):
+    host = models.CharField(max_length=100, default="")
+    path =  models.CharField(max_length=255, default="")
+    method = models.CharField(max_length=20, default="")
+    runtime = models.DateTimeField(auto_now_add=True)
