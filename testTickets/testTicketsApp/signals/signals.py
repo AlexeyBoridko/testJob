a471da62a0a148f3930546b5e46c74b5d32f5f47
AlexeyBoridko <oleksiyua@gmail.com>
2013-10-23 12:31:11 +0300

    ticket:13 - added good checking for db model exists

--- testTickets/testTicketsApp/signals/signals.py
+++ testTickets/testTicketsApp/signals/signals.py
@@ -1,27 +1,24 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from testTicketsApp import models
+from django.db.models import get_models
+from testTicketsApp.models import ModelChangesLog
 
 
 @receiver([post_save, post_delete])
 def model_changes_log(sender, **kwargs):
-
     db_model_name = sender.__name__
 
-    if db_model_name == "ModelChangesLog":
+    #we don't attach to self changes
+    #make sure that ModelChangesLog created after syncdb
+    if db_model_name == "ModelChangesLog" or not ModelChangesLog in get_models("testTicketsApp"):
         return
 
-    # make sure that ModelChangesLog created after syncdb
-    try:
-        action = 'deleted'
+    action = 'deleted'
 
-        # Check changes on created or updated. I know it is not good idea :(
-        if 'created' in kwargs:
-            action = 'updated'
-            if kwargs['created']:
-                action = 'created'
+    # Detect action 'created/updated/deleted'
+    if 'created' in kwargs:
+        action = 'updated'
+        if kwargs['created']:
+            action = 'created'
 
-        models.ModelChangesLog.objects.create(action_type=action, model_name=db_model_name)
-    except:
-        return
-
+    ModelChangesLog.objects.create(action_type=action, model_name=db_model_name)
