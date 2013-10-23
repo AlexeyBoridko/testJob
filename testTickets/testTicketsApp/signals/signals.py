0e159f247cfc0ff6d572c9ded399e05a3c0beb78
AlexeyBoridko <oleksiyua@gmail.com>
2013-10-23 12:54:06 +0300

    ticket:13 - revert 'try  except'

--- testTickets/testTicketsApp/signals/signals.py
+++ testTickets/testTicketsApp/signals/signals.py
@@ -1,24 +1,25 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from django.db.models import get_models
 from testTicketsApp.models import ModelChangesLog
 
 
 @receiver([post_save, post_delete])
 def model_changes_log(sender, **kwargs):
+
     db_model_name = sender.__name__
 
-    #we don't attach to self changes
-    #make sure that ModelChangesLog created after syncdb
-    if db_model_name == "ModelChangesLog" or not ModelChangesLog in get_models("testTicketsApp"):
+    if db_model_name == "ModelChangesLog":
         return
+    # make sure that ModelChangesLog created after syncdb
+    try:
+        action = 'deleted'
 
-    action = 'deleted'
+       # Check changes on created or updated. I know it is not good idea :(
+        if 'created' in kwargs:
+            action = 'updated'
+            if kwargs['created']:
+                action = 'created'
 
-    # Detect action 'created/updated/deleted'
-    if 'created' in kwargs:
-        action = 'updated'
-        if kwargs['created']:
-            action = 'created'
-
-    ModelChangesLog.objects.create(action_type=action, model_name=db_model_name)
+        ModelChangesLog.objects.create(action_type=action, model_name=db_model_name)
+    except:
+        return
